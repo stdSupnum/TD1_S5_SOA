c16e2397dd8606d92851736973796db26331cf09
stdSupnum <23006@supnum.mr>
2025-11-18 16:35:53 +0000

    Commit initial

---
+++ src/main/java/com/supnum/td1/Td1Application.java
@@ -0,0 +1,13 @@
+package com.supnum.td1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/supnum/td1/Td1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supnum.td1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
