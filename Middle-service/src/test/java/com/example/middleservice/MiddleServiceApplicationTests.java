22ada9b2fe5ea3b9f9b1d837ea86c03edb2d4615
stdSupnum <23006@supnum.mr>
2026-01-01 19:58:34 +0000

    initial commit Middle-service

---
+++ Middle-service/src/test/java/com/example/middleservice/MiddleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.middleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
